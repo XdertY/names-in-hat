a329c84566958754ac68f7bdaef2f0b2187e7048
XdertY <axelgron@gmail.com>
2020-12-16 12:53:51 +0200

    Block 2nd attempt to draw

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,29 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-let names = ["Асен", 'Никол', 'Стефчо', 'Тони', 'Алекс']
+let names = ["Асен", 'Никол', 'Стефчо', 'Тони', 'Алекс'];
+let players = [0, 0, 0, 0, 0];
+
 const url = require("url")
 
 
 export default (req, res) => {
   const yourNameKey = url.parse(req.url,true).query.name;
-  let yourName = "";
-  switch(yourName) {
-    case 1 : yourName = "Асен"; break;
-    case 2 : yourName = "Никол"; break;
-    case 3 : yourName = "Стефчо"; break;
-    case 4 : yourName = "Тони"; break;
-    case 5 : yourName = "Алекс"; break;
-
+  if(players[parseInt(yourNameKey) - 1] === 1) {
+    res.statusCode = 200;
+    res.json({name: "Вече си теглил"});
   }
-  let currNames = names.map(el => {
-    if(el !== yourName) return el;
-  })
-  const index = Math.floor(Math.random()*4);
-  const name  = currNames[index];
-  names = names.map(el => {
-    if(el !== name) return el;
-  })
-  names.splice(index, 1);
-  res.statusCode = 200;
-  res.json({ name });
-  console.log(index, names);
+  else {
+    let yourName = "";
+    switch(yourNameKey) {
+      case '1' : yourName = "Асен"; players[0] = 1; break;
+      case '2' : yourName = "Никол"; players[1] = 1; break;
+      case '3' : yourName = "Стефчо"; players[2] = 1 ;break;
+      case '4' : yourName = "Тони"; players[3] = 1 ;break;
+      case '5' : yourName = "Алекс"; players[4] = 1; break;
+
+    }
+    let currNames = names.map(el => {
+      if(el !== yourName) return el;
+    })
+    const index = Math.floor(Math.random()*4);
+    const name  = currNames[index];
+    names = names.map(el => {
+      if(el !== name) return el;
+    })
+    names.splice(index, 1);
+    res.statusCode = 200;
+    res.json({ name });
+    console.log(index, names);
+  } 
 }
