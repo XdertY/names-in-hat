0ffd14d1369ad168b9b9bf6acfd95e6ad9c48452
XdertY <axelgron@gmail.com>
2020-12-16 11:32:50 +0200

    First commit

---
+++ components/card.js
@@ -0,0 +1,58 @@
+import {useState} from 'react'
+import classes from '../styles/Card.module.css'
+import { Dropdown } from 'primereact/dropdown'
+
+
+const Card = (props) => {
+// {/* <input placeholder="Напиши твоето име" value={yourName} onChange={(e) => setYourName(e.target.value)}></input> */}
+    const [name, setName] = useState("");
+    const [disableButton, setDisableButton] = useState(false);
+    const [yourName, setYourName] = useState("");
+
+    const names = [
+        {label: "Алекс", key: 1},
+        {label: "Никол", key: 2},
+        {label: "Асен", key: 3},
+        {label: "Стефчо", key: 4},
+        {label: "Тони", key: 5}
+    ]
+
+    const handleGenerate = () => {
+        document.getElementById("card").classList.toggle(classes.theCardActive);
+        setTimeout(() => {
+            fetch(`http://localhost:3000/api/hello?name=${yourName.label}`).then(response => response.json())
+            .then(data => {
+                setDisableButton(true);
+                setName(data.name)
+            });
+        }, 10000)
+        
+       
+
+    }
+
+    return (
+        <>
+            <div className={classes.mainContainer}>
+
+                <div id="card" className={classes.theCard}>
+                    <div className={classes.theFront}>
+                        <span className={classes.text}>
+                            {name}
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className={classes.buttonHolder}>
+                <Dropdown value={yourName} style={{width: "100%"}} placeholder="Избери името си" options={names} onChange={(e) => setYourName(e.value)}/>
+                
+                <button disabled={disableButton} className={classes.button} onClick={() => handleGenerate()}>Генерирай име</button>
+            </div>
+        </>
+    )
+}
+
+export default Card;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+let names = ["Асен", 'Никол', 'Стефчо', 'Тони', 'Алекс']
+const url = require("url")
+
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  const yourName = url.parse(req.url,true).query.name;
+  let currNames = names.map(el => {
+    if(el !== yourName) return el;
+  })
+  const index = Math.floor(Math.random()*4);
+  const name  = currNames[index];
+  names = names.map(el => {
+    if(el !== name) return el;
+  })
+  names.splice(index, 1);
+  res.statusCode = 200;
+  res.json({ name });
+  console.log(index, names);
 }
