0faaf7c0a0a80a99975fe738684bbecc675bf3c6
XdertY <axelgron@gmail.com>
2020-12-16 11:40:27 +0200

    Changet title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div >
       <Head>
-        <title>Create Next App</title>
+        <title>Names in hat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
