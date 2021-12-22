15da9880011fd11250657ed848c6c5ca09b8cd50
Aleksandar Bakalov <axelgron@gmail.com>
2021-12-22 14:05:49 +0200

    Added firebase configuration

---
+++ firebase/clientApp.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvyD6SZbffVdtaxjgXzx5prHRyzupGBmg",
+  authDomain: "namesinhat-325be.firebaseapp.com",
+  projectId: "namesinhat-325be",
+  storageBucket: "namesinhat-325be.appspot.com",
+  messagingSenderId: "423924204975",
+  appId: "1:423924204975:web:3c1a82dd88fe52cebb1b5a"
+};
+
+export const firebase = initializeApp(firebaseConfig);
